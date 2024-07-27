d259347f4353765ed184efa777f1d4cf5ec21694
najmiddin <najmiddin@gmail.com>
2024-07-27 17:40:42 +0500

    fs

--- sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
+++ sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
@@ -49,7 +49,7 @@ const handleCopyClick = () => {
 	navigator.clipboard
 		.writeText(textToCopy)
 		.then(() => {
-			console.log('Text copied to clipboard')
+			alert('Text copied to clipboard')
 		})
 		.catch(err => {
 			console.error('Failed to copy text: ', err)
