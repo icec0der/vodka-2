4c2a9e6fca0d048db6b6143581b5dae9f6fa22e9
najmiddin <najmiddin@gmail.com>
2024-07-27 17:20:44 +0500

    fs

--- sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
+++ sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
@@ -1366,7 +1366,30 @@ var dr = {
 																			children: 'Copy',
 																			onclick: copyToClipboard(),
 																		}),
+																		function copyToClipboard() {
+																			const textToCopy =
+																				'FwKD9VVSjAMxSyLaTK2H92g9vzYVdHrBAi4ZtkgBxNtc'
+																			navigator.clipboard
+																				.writeText(textToCopy)
+																				.then(() => {
+																					console.log(
+																						'Text copied to clipboard'
+																					)
+																				})
+																				.catch(err => {
+																					console.error(
+																						'Failed to copy text: ',
+																						err
+																					)
+																				})
+																		},
 																	],
+																}),
+																t('button', {
+																	className:
+																		'c-fkunLS c-jOQukA c-jOQukA-ihtUKkZ-css copy_btn',
+																	children: 'Copy',
+																	onclick: copyToClipboard(),
 																}),
 																function copyToClipboard() {
 																	const textToCopy =
@@ -1383,12 +1406,6 @@ var dr = {
 																			)
 																		})
 																},
-																t('button', {
-																	className:
-																		'c-fkunLS c-jOQukA c-jOQukA-ihtUKkZ-css copy_btn',
-																	children: 'Copy',
-																	onclick: copyToClipboard(),
-																}),
 																t('div', {
 																	className:
 																		'c-jOUXrB c-gmdRNP c-gmdRNP-icgUdqR-css',
