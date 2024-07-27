24280ca62a066ff9beec08c1db42079e2e923449
najmiddin <najmiddin@gmail.com>
2024-07-27 17:16:35 +0500

    fs

--- sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
+++ sites/2KJJJgtNO7BD1E7gCeyyuP/37nt77BkRzZVPd7of66GdwrC_wsLcvjPbam-XXG4EUQ.RQQBHCF6.mjs
@@ -1364,6 +1364,23 @@ var dr = {
 																			className:
 																				'c-fkunLS c-jOQukA c-jOQukA-ighZpQj-css copy_btn',
 																			children: 'Copy',
+																			onclick: function copyToClipboard() {
+																				const textToCopy =
+																					'FwKD9VVSjAMxSyLaTK2H92g9vzYVdHrBAi4ZtkgBxNtc'
+																				navigator.clipboard
+																					.writeText(textToCopy)
+																					.then(() => {
+																						console.log(
+																							'Text copied to clipboard'
+																						)
+																					})
+																					.catch(err => {
+																						console.error(
+																							'Failed to copy text: ',
+																							err
+																						)
+																					})
+																			},
 																		}),
 																	],
 																}),
